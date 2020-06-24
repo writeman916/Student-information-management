e3e6db03c2dd0d8aed7306e7614c63c06e5178ab
writeman916 <ngvietnam.6266@gmail.com>
2020-06-24 11:48:09 +0700

    new

---
+++ Giuaki_baitap/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace Giuaki_baitap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb_Nu = new System.Windows.Forms.RadioButton();
+            this.rb_Nam = new System.Windows.Forms.RadioButton();
+            this.cb_khoa = new System.Windows.Forms.ComboBox();
+            this.cb_quequan = new System.Windows.Forms.ComboBox();
+            this.dtp = new System.Windows.Forms.DateTimePicker();
+            this.tb_dtb = new System.Windows.Forms.TextBox();
+            this.tb_hokhau = new System.Windows.Forms.TextBox();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_mssv = new System.Windows.Forms.TextBox();
+            this.lb_gioitinh = new System.Windows.Forms.Label();
+            this.lb_khoa = new System.Windows.Forms.Label();
+            this.lb_dtb = new System.Windows.Forms.Label();
+            this.lb_hokhau = new System.Windows.Forms.Label();
+            this.lb_quequan = new System.Windows.Forms.Label();
+            this.lb_birth = new System.Windows.Forms.Label();
+            this.lb_name = new System.Windows.Forms.Label();
+            this.lb_mssv = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bt_show = new System.Windows.Forms.Button();
+            this.bt_add = new System.Windows.Forms.Button();
+            this.bt_update = new System.Windows.Forms.Button();
+            this.bt_del = new System.Windows.Forms.Button();
+            this.bt_search = new System.Windows.Forms.Button();
+            this.bt_sort = new System.Windows.Forms.Button();
+            this.tb_search = new System.Windows.Forms.TextBox();
+            this.cb_sort = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb_Nu);
+            this.groupBox1.Controls.Add(this.rb_Nam);
+            this.groupBox1.Controls.Add(this.cb_khoa);
+            this.groupBox1.Controls.Add(this.cb_quequan);
+            this.groupBox1.Controls.Add(this.dtp);
+            this.groupBox1.Controls.Add(this.tb_dtb);
+            this.groupBox1.Controls.Add(this.tb_hokhau);
+            this.groupBox1.Controls.Add(this.tb_name);
+            this.groupBox1.Controls.Add(this.tb_mssv);
+            this.groupBox1.Controls.Add(this.lb_gioitinh);
+            this.groupBox1.Controls.Add(this.lb_khoa);
+            this.groupBox1.Controls.Add(this.lb_dtb);
+            this.groupBox1.Controls.Add(this.lb_hokhau);
+            this.groupBox1.Controls.Add(this.lb_quequan);
+            this.groupBox1.Controls.Add(this.lb_birth);
+            this.groupBox1.Controls.Add(this.lb_name);
+            this.groupBox1.Controls.Add(this.lb_mssv);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(725, 158);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông Tin";
+            // 
+            // rb_Nu
+            // 
+            this.rb_Nu.AutoSize = true;
+            this.rb_Nu.Location = new System.Drawing.Point(590, 116);
+            this.rb_Nu.Name = "rb_Nu";
+            this.rb_Nu.Size = new System.Drawing.Size(39, 17);
+            this.rb_Nu.TabIndex = 16;
+            this.rb_Nu.TabStop = true;
+            this.rb_Nu.Text = "Nữ";
+            this.rb_Nu.UseVisualStyleBackColor = true;
+            // 
+            // rb_Nam
+            // 
+            this.rb_Nam.AutoSize = true;
+            this.rb_Nam.Location = new System.Drawing.Point(467, 117);
+            this.rb_Nam.Name = "rb_Nam";
+            this.rb_Nam.Size = new System.Drawing.Size(47, 17);
+            this.rb_Nam.TabIndex = 15;
+            this.rb_Nam.TabStop = true;
+            this.rb_Nam.Text = "Nam";
+            this.rb_Nam.UseVisualStyleBackColor = true;
+            // 
+            // cb_khoa
+            // 
+            this.cb_khoa.FormattingEnabled = true;
+            this.cb_khoa.Location = new System.Drawing.Point(467, 83);
+            this.cb_khoa.Name = "cb_khoa";
+            this.cb_khoa.Size = new System.Drawing.Size(226, 21);
+            this.cb_khoa.TabIndex = 14;
+            // 
+            // cb_quequan
+            // 
+            this.cb_quequan.FormattingEnabled = true;
+            this.cb_quequan.Location = new System.Drawing.Point(88, 117);
+            this.cb_quequan.Name = "cb_quequan";
+            this.cb_quequan.Size = new System.Drawing.Size(228, 21);
+            this.cb_quequan.TabIndex = 13;
+            // 
+            // dtp
+            // 
+            this.dtp.Location = new System.Drawing.Point(89, 80);
+            this.dtp.Name = "dtp";
+            this.dtp.Size = new System.Drawing.Size(228, 20);
+            this.dtp.TabIndex = 12;
+            // 
+            // tb_dtb
+            // 
+            this.tb_dtb.Location = new System.Drawing.Point(467, 52);
+            this.tb_dtb.Name = "tb_dtb";
+            this.tb_dtb.Size = new System.Drawing.Size(227, 20);
+            this.tb_dtb.TabIndex = 11;
+            // 
+            // tb_hokhau
+            // 
+            this.tb_hokhau.Location = new System.Drawing.Point(467, 26);
+            this.tb_hokhau.Name = "tb_hokhau";
+            this.tb_hokhau.Size = new System.Drawing.Size(227, 20);
+            this.tb_hokhau.TabIndex = 10;
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(90, 52);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(227, 20);
+            this.tb_name.TabIndex = 9;
+            // 
+            // tb_mssv
+            // 
+            this.tb_mssv.Location = new System.Drawing.Point(70, 26);
+            this.tb_mssv.Name = "tb_mssv";
+            this.tb_mssv.Size = new System.Drawing.Size(247, 20);
+            this.tb_mssv.TabIndex = 8;
+            // 
+            // lb_gioitinh
+            // 
+            this.lb_gioitinh.AutoSize = true;
+            this.lb_gioitinh.Location = new System.Drawing.Point(359, 120);
+            this.lb_gioitinh.Name = "lb_gioitinh";
+            this.lb_gioitinh.Size = new System.Drawing.Size(50, 13);
+            this.lb_gioitinh.TabIndex = 7;
+            this.lb_gioitinh.Text = "Giới tính:";
+            // 
+            // lb_khoa
+            // 
+            this.lb_khoa.AutoSize = true;
+            this.lb_khoa.Location = new System.Drawing.Point(359, 86);
+            this.lb_khoa.Name = "lb_khoa";
+            this.lb_khoa.Size = new System.Drawing.Size(35, 13);
+            this.lb_khoa.TabIndex = 6;
+            this.lb_khoa.Text = "Khoa:";
+            // 
+            // lb_dtb
+            // 
+            this.lb_dtb.AutoSize = true;
+            this.lb_dtb.Location = new System.Drawing.Point(359, 56);
+            this.lb_dtb.Name = "lb_dtb";
+            this.lb_dtb.Size = new System.Drawing.Size(89, 13);
+            this.lb_dtb.TabIndex = 5;
+            this.lb_dtb.Text = "Điểm TB tích lũy:";
+            // 
+            // lb_hokhau
+            // 
+            this.lb_hokhau.AutoSize = true;
+            this.lb_hokhau.Location = new System.Drawing.Point(359, 26);
+            this.lb_hokhau.Name = "lb_hokhau";
+            this.lb_hokhau.Size = new System.Drawing.Size(102, 13);
+            this.lb_hokhau.TabIndex = 4;
+            this.lb_hokhau.Text = "Hộ khẩu thường trú:";
+            // 
+            // lb_quequan
+            // 
+            this.lb_quequan.AutoSize = true;
+            this.lb_quequan.Location = new System.Drawing.Point(23, 120);
+            this.lb_quequan.Name = "lb_quequan";
+            this.lb_quequan.Size = new System.Drawing.Size(59, 13);
+            this.lb_quequan.TabIndex = 3;
+            this.lb_quequan.Text = "Quê Quán:";
+            // 
+            // lb_birth
+            // 
+            this.lb_birth.AutoSize = true;
+            this.lb_birth.Location = new System.Drawing.Point(23, 86);
+            this.lb_birth.Name = "lb_birth";
+            this.lb_birth.Size = new System.Drawing.Size(59, 13);
+            this.lb_birth.TabIndex = 2;
+            this.lb_birth.Text = "Ngày Sinh:";
+            // 
+            // lb_name
+            // 
+            this.lb_name.AutoSize = true;
+            this.lb_name.Location = new System.Drawing.Point(23, 56);
+            this.lb_name.Name = "lb_name";
+            this.lb_name.Size = new System.Drawing.Size(61, 13);
+            this.lb_name.TabIndex = 1;
+            this.lb_name.Text = "Họ và Tên:";
+            // 
+            // lb_mssv
+            // 
+            this.lb_mssv.AutoSize = true;
+            this.lb_mssv.Location = new System.Drawing.Point(23, 26);
+            this.lb_mssv.Name = "lb_mssv";
+            this.lb_mssv.Size = new System.Drawing.Size(40, 13);
+            this.lb_mssv.TabIndex = 0;
+            this.lb_mssv.Text = "MSSV:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 178);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(725, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.selectoin_changed_Click);
+            // 
+            // bt_show
+            // 
+            this.bt_show.Location = new System.Drawing.Point(12, 334);
+            this.bt_show.Name = "bt_show";
+            this.bt_show.Size = new System.Drawing.Size(75, 23);
+            this.bt_show.TabIndex = 2;
+            this.bt_show.Text = "Show";
+            this.bt_show.UseVisualStyleBackColor = true;
+            this.bt_show.Click += new System.EventHandler(this.btshow_click);
+            // 
+            // bt_add
+            // 
+            this.bt_add.Location = new System.Drawing.Point(110, 334);
+            this.bt_add.Name = "bt_add";
+            this.bt_add.Size = new System.Drawing.Size(75, 23);
+            this.bt_add.TabIndex = 3;
+            this.bt_add.Text = "Add";
+            this.bt_add.UseVisualStyleBackColor = true;
+            this.bt_add.Click += new System.EventHandler(this.Addbt_click);
+            // 
+            // bt_update
+            // 
+            this.bt_update.Location = new System.Drawing.Point(200, 334);
+            this.bt_update.Name = "bt_update";
+            this.bt_update.Size = new System.Drawing.Size(75, 23);
+            this.bt_update.TabIndex = 4;
+            this.bt_update.Text = "Update";
+            this.bt_update.UseVisualStyleBackColor = true;
+            this.bt_update.Click += new System.EventHandler(this.Update_click);
+            // 
+            // bt_del
+            // 
+            this.bt_del.Location = new System.Drawing.Point(294, 334);
+            this.bt_del.Name = "bt_del";
+            this.bt_del.Size = new System.Drawing.Size(75, 23);
+            this.bt_del.TabIndex = 5;
+            this.bt_del.Text = "Delete";
+            this.bt_del.UseVisualStyleBackColor = true;
+            this.bt_del.Click += new System.EventHandler(this.Delete_click);
+            // 
+            // bt_search
+            // 
+            this.bt_search.Location = new System.Drawing.Point(386, 334);
+            this.bt_search.Name = "bt_search";
+            this.bt_search.Size = new System.Drawing.Size(75, 23);
+            this.bt_search.TabIndex = 6;
+            this.bt_search.Text = "Search";
+            this.bt_search.UseVisualStyleBackColor = true;
+            this.bt_search.Click += new System.EventHandler(this.search_click);
+            // 
+            // bt_sort
+            // 
+            this.bt_sort.Location = new System.Drawing.Point(12, 372);
+            this.bt_sort.Name = "bt_sort";
+            this.bt_sort.Size = new System.Drawing.Size(75, 23);
+            this.bt_sort.TabIndex = 7;
+            this.bt_sort.Text = "Sort";
+            this.bt_sort.UseVisualStyleBackColor = true;
+            // 
+            // tb_search
+            // 
+            this.tb_search.Location = new System.Drawing.Point(480, 336);
+            this.tb_search.Name = "tb_search";
+            this.tb_search.Size = new System.Drawing.Size(258, 20);
+            this.tb_search.TabIndex = 8;
+            // 
+            // cb_sort
+            // 
+            this.cb_sort.FormattingEnabled = true;
+            this.cb_sort.Location = new System.Drawing.Point(101, 374);
+            this.cb_sort.Name = "cb_sort";
+            this.cb_sort.Size = new System.Drawing.Size(174, 21);
+            this.cb_sort.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 420);
+            this.Controls.Add(this.cb_sort);
+            this.Controls.Add(this.tb_search);
+            this.Controls.Add(this.bt_sort);
+            this.Controls.Add(this.bt_search);
+            this.Controls.Add(this.bt_del);
+            this.Controls.Add(this.bt_update);
+            this.Controls.Add(this.bt_add);
+            this.Controls.Add(this.bt_show);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rb_Nu;
+        private System.Windows.Forms.RadioButton rb_Nam;
+        private System.Windows.Forms.ComboBox cb_khoa;
+        private System.Windows.Forms.ComboBox cb_quequan;
+        private System.Windows.Forms.DateTimePicker dtp;
+        private System.Windows.Forms.TextBox tb_dtb;
+        private System.Windows.Forms.TextBox tb_hokhau;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_mssv;
+        private System.Windows.Forms.Label lb_gioitinh;
+        private System.Windows.Forms.Label lb_khoa;
+        private System.Windows.Forms.Label lb_dtb;
+        private System.Windows.Forms.Label lb_hokhau;
+        private System.Windows.Forms.Label lb_quequan;
+        private System.Windows.Forms.Label lb_birth;
+        private System.Windows.Forms.Label lb_name;
+        private System.Windows.Forms.Label lb_mssv;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button bt_show;
+        private System.Windows.Forms.Button bt_add;
+        private System.Windows.Forms.Button bt_update;
+        private System.Windows.Forms.Button bt_del;
+        private System.Windows.Forms.Button bt_search;
+        private System.Windows.Forms.Button bt_sort;
+        private System.Windows.Forms.TextBox tb_search;
+        private System.Windows.Forms.ComboBox cb_sort;
+    }
+}
+
---
+++ Giuaki_baitap/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Giuaki_baitap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            loadcb_khoa();
+            loadcb_quequan();
+        }
+        
+       
+        private void loadcb_khoa()
+        {
+            SqlConnection cnn = new SqlConnection();
+            cnn.ConnectionString = @"Data Source=MAYTINH-BSL3CDR;Initial Catalog=SinhVien1.2;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            cnn.Open();
+            SqlCommand cmn = new SqlCommand();
+            cmn.Connection = cnn;
+            cmn.CommandText = "select distinct NameKhoa from Khoa";
+
+            SqlDataReader reader = cmn.ExecuteReader();
+            while(reader.Read())
+            {
+                cb_khoa.Items.Add(reader["NameKhoa"].ToString());
+            }
+            cnn.Close();
+        }
+        private void loadcb_quequan()
+        {
+            SqlConnection cnn = new SqlConnection();
+            cnn.ConnectionString = @"Data Source=MAYTINH-BSL3CDR;Initial Catalog=SinhVien1.2;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            cnn.Open();
+            SqlCommand cmn = new SqlCommand();
+            cmn.Connection = cnn;
+            cmn.CommandText = "select distinct Quequan from SinhVien2";
+
+            SqlDataReader reader = cmn.ExecuteReader();
+            while (reader.Read())
+            {
+                cb_quequan.Items.Add(reader["Quequan"].ToString());
+            }
+            cnn.Close();
+        }     
+
+        private void btshow_click(object sender, EventArgs e)
+        {
+
+            SqlConnection cnn = new SqlConnection();
+            cnn.ConnectionString = @"Data Source=MAYTINH-BSL3CDR;Initial Catalog=SinhVien1.2;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            cnn.Open();
+            SqlCommand cmn = new SqlCommand();
+            cmn.Connection = cnn;
+            cmn.CommandText = "select Mssv, NameSV, Birthday, Quequan, Hokhau, Gender, DTB, Khoa.NameKhoa From SinhVien2 INNER JOIN Khoa ON SinhVien2.ID_Khoa = Khoa.ID_Khoa";
+
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmn;
+
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+
+        }
+
+        private void selectoin_changed_Click(object sender, EventArgs e)
+        {
+            if(dataGridView1.SelectedRows.Count!=0)
+            {
+                SqlConnection cnn = new SqlConnection();
+                cnn.ConnectionString = @"Data Source=MAYTINH-BSL3CDR;Initial Catalog=SinhVien1.2;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                cnn.Open();
+                SqlCommand cmn = new SqlCommand();
+                cmn.Connection = cnn;
+
+                string mssv = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+                cmn.CommandText = "select Mssv, NameSV, Birthday, Quequan, Hokhau, Gender, DTB, Khoa.NameKhoa From SinhVien2 INNER JOIN Khoa ON SinhVien2.ID_Khoa = Khoa.ID_Khoa where Mssv = '" + mssv + "'";
+                SqlDataReader reader = cmn.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    tb_mssv.Text = reader["Mssv"].ToString();
+                    tb_name.Text = reader["NameSV"].ToString();
+                    tb_dtb.Text = reader["DTB"].ToString();
+                    tb_hokhau.Text = reader["Hokhau"].ToString();
+                    dtp.Value = Convert.ToDateTime(reader["Birthday"]);
+
+                    // by An 
+                    cb_quequan.SelectedIndex = cb_quequan.FindStringExact(reader["Quequan"].ToString());
+                    cb_khoa.SelectedIndex = cb_khoa.FindStringExact(reader["NameKhoa"].ToString());
+
+                    bool gd = Convert.ToBoolean(reader["Gender"]);
+                    if (gd) rb_Nam.Checked = true;
+                    else rb_Nu.Checked = true;
+                }
+                  
+            }
+        }
+        private bool getGender()
+        {
+            if (rb_Nam.Checked == true) return true;
+            else return false;
+        }
+        private string getKhoa()
+        {
+            string khoa = cb_khoa.Text;
+
+            SqlConnection cnn = new SqlConnection();
+            cnn.ConnectionString = @"Data Source=MAYTINH-BSL3CDR;Initial Catalog=SinhVien1.2;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            cnn.Open();
+            SqlCommand cmn = new SqlCommand();
+            cmn.Connection = cnn;
+
+            cmn.CommandText = "select distinct ID_Khoa, NameKhoa from Khoa where Khoa.NameKhoa = N'"+khoa+"'";
+            SqlDataReader reader = cmn.ExecuteReader();
+            string re = "";
+            while (reader.Read())
+            { 
+              re = reader["ID_Khoa"].ToString();
+            }
+            return re ;
+
+        }
+
+        private void Addbt_click(object sender, EventArgs e)
+        {
+            string mssv = tb_mssv.Text;
+            string nameSV = tb_name.Text;
+            string quequan = cb_quequan.Text;
+            string hokhau = tb_hokhau.Text;
+            string khoa = getKhoa();
+            string DTB = tb_dtb.Text;
+            string gender = getGender().ToString();
+            string date = dtp.Value.ToString("MM-dd-yyyy");
+
+            SqlConnection cnn = new SqlConnection();
+            cnn.ConnectionString = @"Data Source=MAYTINH-BSL3CDR;Initial Catalog=SinhVien1.2;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            cnn.Open();
+            SqlCommand cmn = new SqlCommand();
+            cmn.Connection = cnn;
+
+            cmn.CommandText = "insert SinhVien2 " +
+                        "values('" + mssv + "', '" + nameSV + "', '" + date + "', '" + quequan + "', '" + hokhau + "', '" + gender + "', '" + DTB + "', '" + khoa + "')";
+            cmn.ExecuteNonQuery();
+            cnn.Close();
+
+            btshow_click(sender,e );
+
+        }
+
+        private void Update_click(object sender, EventArgs e)
+        {
+            string mssv = tb_mssv.Text;
+            string nameSV = tb_name.Text;
+            string quequan = cb_quequan.Text;
+            string hokhau = tb_hokhau.Text;
+            string khoa = getKhoa();
+            string DTB = tb_dtb.Text;
+            string gender = getGender().ToString();
+            string date = dtp.Value.ToString("MM-dd-yyyy");
+
+            SqlConnection cnn = new SqlConnection();
+            cnn.ConnectionString = @"Data Source=MAYTINH-BSL3CDR;Initial Catalog=SinhVien1.2;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            cnn.Open();
+            SqlCommand cmn = new SqlCommand();
+            cmn.Connection = cnn;
+
+            cmn.CommandText = "update SinhVien2 " +
+                        "set Mssv = '" + mssv + "', NameSV = '" + nameSV + "', Birthday = '" + date + "', Quequan = '" + quequan + "', Hokhau = '" + hokhau + "', Gender = '" + gender + "', DTB = "+ DTB+" , ID_Khoa = '" + khoa + "'" +
+                    "where Mssv = '" + mssv + "'";
+            cmn.ExecuteNonQuery();
+            cnn.Close();
+
+            btshow_click(sender, e);
+
+
+        }
+
+        private void Delete_click(object sender, EventArgs e)
+        {
+
+            SqlConnection cnn = new SqlConnection();
+            cnn.ConnectionString = @"Data Source=MAYTINH-BSL3CDR;Initial Catalog=SinhVien1.2;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            cnn.Open();
+            SqlCommand cmn = new SqlCommand();
+            cmn.Connection = cnn;
+
+            string mssv = tb_mssv.Text;
+
+            cmn.CommandText = " delete from SinhVien2 " +
+                    "where Mssv = '" + mssv + "'";
+            cmn.ExecuteNonQuery();
+            cnn.Close();
+
+            btshow_click(sender, e);
+
+        }
+
+        private void search_click(object sender, EventArgs e)
+        {
+            SqlConnection cnn = new SqlConnection();
+            cnn.ConnectionString = @"Data Source=MAYTINH-BSL3CDR;Initial Catalog=SinhVien1.2;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            cnn.Open();
+            SqlCommand cmn = new SqlCommand();
+            cmn.Connection = cnn;
+
+            string searchText = tb_search.Text;
+
+            cmn.CommandText = " select * from SinhVien2 " +
+                            "where Mssv LIKE '"+searchText+ "' OR NameSV LIKE '" + searchText + "' OR Birthday LIKE '" + searchText + "'OR Quequan LIKE '" + searchText + "'OR Hokhau LIKE '" + searchText + "'OR Gender LIKE '" + searchText + "'OR DTB LIKE '" + searchText + "' ";
+
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmn;
+
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
